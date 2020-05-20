9bb36fee7e5c947b33a39e282f919d5387597343
MuhdAliuddin <edmars.161177@gmail.com>
2020-05-20 14:17:08 +0800

    First Commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel = "stylesheet" type = "text/css" href = "stylesheets/css/style.css"/>
+
+    <!-- GOOGLE FONTS
+         #POPPINS 300,400,700 -thin/regular/bold
+    -->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+
+    <body>
+        <!--NAVBAR MENU -->
+        <?php include "navbar.php" ?>
+        
+        <header class="border-white">
+            <div class="">
+                <h4 class="align-center fade-in"><span class="bold-upprcase">Hello there</span>, my name is aliuddin.</h4>
+                <h6 class="align-center italic fade-in">an aspiring Junior Web Developer<h6>
+            </div>
+        </header>
+
+        <main class="center-flex">
+            <div class="btn-round">
+                <a href="docs/CV_MUHAMMAD_ALIUDDIN.pdf" class="btn-text" target="_blank">view portfolio</a>
+            </div>
+
+            <div class="btn-round">
+                <a href="docs/RESUME_MUHAMMAD_ALIUDDIN.pdf" class="btn-text" target="_blank">view resume</a>
+            </div>
+
+        </main>
+
+        <footer class="right-flex">
+            <div>
+                <a href="https://github.com/MuhdAliuddin" class="" target="_blank"><img src="image/github.png" alt="github-logo" class="img-size"></a>
+                <p class="d-block d-sm-none">Visit my Github repo</p>
+            </div>
+            <div>
+                <a href="https://www.linkedin.com/in/aliuddin0397/" class="" target="_blank"><img src="image/linkedin.png" alt="linkedin-logo" class="img-size"></a>
+                <p class="d-block d-sm-none">Learn more about me at LinkedIn</p>
+            </div>
+        </footer>
+
+    
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    </body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,10 @@
+<!--NAVBAR MENU -->
+<nav class="navbar">
+    <a href="index.php" class="navbar-brand nav-deco-white nav-home">mars.</a>
+    <ul>
+        <li>
+            <a href="mailto: hello@mdaliuddin.com?subject=Hello There!" class="nav-deco-grey color-grey" href="index.php">contact me</a>
+        </li>
+    </ul>
+</nav>
+
